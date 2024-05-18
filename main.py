70620a1171deea49dffd1d9ec14ab03f986400d4
Alex <shantemgardenix@gmail.com>
2024-05-18 21:24:17 +0500

    feat: shooting method with bisection and 2nd order adams

--- main.py
+++ main.py
@@ -1,101 +1,75 @@
 import math
+import numpy as np
+from matplotlib import pyplot as plt
 from typing import Callable
 
-import matplotlib.pyplot as plt
+
+def f(x, y):
+    return y + 2.8 + 0.7 * x * (x - 1) ** 2
 
 
-class Method:
-    def __init__(self, title: str, func: Callable):
-        self.title = title
-        self.func = func
+def get_analytical_solution(a: float, b: float, h: float, func: Callable[[float], float]) -> float:
+    return zip(*[(x, func(x)) for x in np.arange(a, b + h, h)])
 
 
-def runge_kutta_3rd_order_precalc(x_0: float, y_0: float, h: float, func: Callable[[float, float], float]) -> float:
-    k1 = h * func(x_0, y_0)
-    k2 = h * func(x_0 + h, y_0 + k1)
-    k3 = h * func(x_0 + h / 2, y_0 + (1 / 4) * k1 + (1 / 4) * k2)
+def cauchy(x0: float, y0: float, z0: float, h: float) -> (float, float):
+    x_next = x0 + h
+    y_next = y0 + h * z0
+    z_next = z0 + h * f(x0 + (h / 2), y0 + (h / 2) * f(x0, y0))
 
-    return y_0 + (1 / 6) * k1 + (1 / 6) * k2 + (4 / 6) * k3
-
-
-def implicit_two_step_adams(a: float, h: float, y_0: float, n: int, func: Callable[[float, float], float]) -> list[
-    float]:
-    result = [y_0, runge_kutta_3rd_order_precalc(0, y_0, h, func)]
-
-    for i in range(1, n):
-        y_n = result[-1]
-        x_n = a + h * i
-
-        result.append((y_n + (8 / 12) * h * func(x_n, y_n) - (1 / 12) * h * func(a + h * (i - 1), result[-2]))
-                      /
-                      (1 - (5 / 12) * h * 50 * ((a + h * (i + 1)) - 0.6) * ((a + h * (i + 1)) - 0.85)))
-
-    return result
+    return x_next, y_next, z_next
 
 
-def explicit_3rd_order_taylor(a: float, h: float, y_0: float, n: int, func: Callable[[float, float], float]) -> list[
-    float]:
-    result = [y_0]
+def explicit_2nd_order_adams(x0: float, mu: float, z0: float, h: float, n: int) -> list[(float, float, float)]:
+    data = [(x0, mu, z0), cauchy(x0, mu, z0, h)]
 
-    for i in range(n):
-        x_i = a + h * i
-        y_i = result[-1]
-        result.append(y_i + h * func(x_i, y_i)
-                      + h * h / 2 * (
-                              100 * x_i * y_i - 72.5 * y_i + func(x_i, y_i) * (50 * x_i * x_i - 72.5 * x_i + 25.5))
-                      + h * h * h / 6 * (100 * y_i + func(x_i, y_i) * (200 * x_i - 145) - 100 * x_i * y_i
-                                         - 72.5 * y_i + func(x_i, y_i) * (50 * x_i * x_i - 72.5 * x_i + 25.5)
-                                         * (-50 * x_i * x_i + 72.5 * x_i - 25.5)))
-    return result
+    for i in range(2, n + 1):
+        x_i = data[-1][0]
+        y_i = data[-1][1]
+        z_i = data[-1][2]
 
+        x_next = x_i + h
+        y_next = y_i + (h / 2) * (3 * z_i - data[-2][2])
+        z_next = z_i + (h / 2) * (3 * f(x_i, y_i) - f(data[-2][0], data[-2][1]))
 
-def cauchy(a: float, h: float, y_0: float, n: int, func: Callable[[float, float], float]) -> list[float]:
-    result = [y_0]
+        data.append((x_next, y_next, z_next))
 
-    for i in range(n):
-        result.append(
-            result[-1] + h * func((a + h * i) + h / 2, result[-1] + h / 2 * func(a + h * i, result[-1])))
-
-    return result
+    return data
 
 
-def get_x_axis(a: float, h: float, n: int) -> list[float]:
-    x_axis = [a]
-    [x_axis.append(a + h * i) for i in range(n)]
+def bisection_method(a: float, b: float, beta: float, x0: float, h: float, z0: float, n: int):
+    result_left = explicit_2nd_order_adams(x0, a, z0, h, n)[-1][2]  # тут имеем z_n
+    result_right = explicit_2nd_order_adams(x0, b, z0, h, n)[-1][2]  # тут имеем z_n
 
-    return x_axis
+    while math.fabs(result_left - result_right) > 1e-9:
+        mid = (a + b) / 2
+        result_mid = explicit_2nd_order_adams(x0, mid, z0, h, n)[-1][2]  # тут имеем z_n
+        if beta > result_mid:
+            a = mid
+            result_left = result_mid
+        else:
+            b = mid
+            result_right = result_mid
 
-
-def get_result(a: float, y_0: float, methods: list[Method], exact: Callable[[float], float]):
-    figure, axis = plt.subplots(2, 2)
-    plt.subplots_adjust(hspace=0.4, wspace=0.3)
-    for idx, pos in enumerate([[0, 0], [0, 1], [1, 0]]):
-        first_y = methods[idx].func(a, 0.1, y_0, 10, lambda x, y: 50 * y * (x - 0.6) * (x - 0.85))
-        second_y = methods[idx].func(a, 0.05, y_0, 20, lambda x, y: 50 * y * (x - 0.6) * (x - 0.85))
-        third_y = methods[idx].func(a, 0.025, y_0, 40, lambda x, y: 50 * y * (x - 0.6) * (x - 0.85))
-
-        first_x = get_x_axis(a, 0.1, 10)
-        second_x = get_x_axis(a, 0.05, 20)
-        third_x = get_x_axis(a, 0.025, 40)
-
-        exact_y = [exact(0)]
-        [exact_y.append(exact(0 + 0.025 * i)) for i in range(40)]
-
-        axis[pos[0], pos[1]].plot(first_x, first_y, color="r")
-        axis[pos[0], pos[1]].plot(second_x, second_y, color="g")
-        axis[pos[0], pos[1]].plot(third_x, third_y, color="b")
-        axis[pos[0], pos[1]].plot(third_x, exact_y, color="black")
-
-        axis[pos[0], pos[1]].title.set_text(methods[idx].title)
-
-    axis[1, 1].axis('off')
-    figure.legend(['0.1', '0.05', '0.025', 'Точное решение'], loc='lower right')
-
-    plt.show()
+    return (a + b) / 2
 
 
-get_result(0, 0.1, [
-    Method('Коши', cauchy),
-    Method('Тейлор явный 3 порядка', explicit_3rd_order_taylor),
-    Method('Адамса двушаговый неявный', implicit_two_step_adams)
-], lambda x: 1 / 10 * math.exp(1 / 12 * x * (200 * x * x - 435 * x + 306)))
+def shooting_method(a: float, b: float, beta: float, x0: float, xn: float, h: float, z0: float):
+    n = int((xn - x0) / h)
+    mu = bisection_method(a, b, beta, x0, h, z0, n)
+    result = explicit_2nd_order_adams(x0, mu, z0, h, n)
+
+    return zip(*[(res[0], res[1]) for res in result])
+
+
+ox, oy = get_analytical_solution(0, 5, 0.05, lambda x: math.exp(x) + math.exp(-x) + x * (-0.7 * x * x + 1.4 * x - 4.9))
+plt.plot(ox, oy, color="b", label="Точное решение")
+
+step = float(input("Введите шаг\n"))
+ox, oy = shooting_method(1, 3, math.exp(5) - math.exp(-5) - 43.4, 0, 5, step, -4.9)
+plt.plot(ox, oy, color="r", label="Метод стрельбы")
+
+plt.title(f"Шаг h = {step}")
+plt.grid()
+plt.legend()
+plt.show()
